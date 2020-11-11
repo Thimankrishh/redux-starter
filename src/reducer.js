7bd7a23ee10444108127957914d729d3096f87d9
Thiman Krishly <thiman.krishly@sprintlabs.lk>
2020-11-11 20:57:35 +0530

    redux-actions-basic

---
+++ src/reducer.js
@@ -0,0 +1,25 @@
+const { lastIndexOf } = require("lodash");
+
+let lastId = 0;
+
+// when we start application redux is calling the reducer and pass the initail state. 
+function reducer(state = [], action){ // reducer is a pure function. 
+
+    if( action.type === 'bugAdded'){
+
+        return [
+            ...state,
+            {
+                id: ++lastId,
+                description: action.payload.description,
+                resolved: false
+            }
+        ];
+    }
+    else if( action.type === 'bugRemoved'){
+
+        return state.filter( bug => bug.id !== action.payload.id);
+    }
+
+    return state;
+}
